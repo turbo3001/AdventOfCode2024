86f4fffa981afe04879332362cb541e877e4e181
turbo3001 <lukerackstraw@hotmail.com>
2024-12-05 23:15:47 +0000

    Modifying copied Template Files with Day Number

--- CreateDay.bat
+++ CreateDay.bat
@@ -1,8 +1,11 @@
-@echo off
-set FolderName=.\Day_%1\
-if exist %FolderName% (
+REM @echo off
+set FolderName=Day_%1
+set FolderPath=.\%FolderName%\
+if exist %FolderPath% (
     echo Folder %FolderName% already exists
     exit
 )
 
-xcopy /s .\TemplateDay\ %FolderName%
+xcopy /s .\TemplateDay\ %FolderPath%
+cd %FolderPath%Src
+powershell -Command "(gc ./Part_1.py) -replace 'Day_X', '%FolderName%' | Out-File -encoding ASCII Part_1.py"
